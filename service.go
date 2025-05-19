bfffa9db9f346f7a59c681757a3a90bbdbe29a51
tarcisio <20189+tarcisio@users.noreply.github.com>
2025-05-19 10:32:19 -0300

    lib updated

--- service.go
+++ service.go
@@ -1,12 +1,16 @@
 package service
 
 import (
-	"context"
-	"log"
-	"time"
+	"log/slog"
 
 	"github.com/opsminded/graphlib/v2"
 )
+
+type VertexAttribute struct {
+	Type        string
+	Description string
+	Value       any
+}
 
 type QueryResult struct {
 	Title     string
@@ -15,112 +19,100 @@ type QueryResult struct {
 }
 
 type Summary struct {
-	TotalEdges        int
-	TotalVertices     int
-	UnhealthyVertices []graphlib.Vertex
-}
-
-type Extractor interface {
-	Frequency() time.Duration
-
-	NextEdge() graphlib.Edge
-	HasNextEdge() bool
-
-	NextVertex() graphlib.Vertex
-	HasNextVertex() bool
-
-	Reset()
+	TotalEdges             int
+	TotalVertices          int
+	TotalHealthyVertices   int
+	TotalUnhealthyVertices int
+	UnhealthyVertices      []graphlib.Vertex
 }
 
 type Service struct {
-	extractors []Extractor
-	graph      *graphlib.Graph
+	graph *graphlib.Graph
 }
 
-func New(graph *graphlib.Graph, extractors []Extractor) *Service {
+func New(graph *graphlib.Graph) *Service {
 	service := &Service{
-		extractors: extractors,
-		graph:      graph,
+		graph: graph,
 	}
 	return service
 }
 
-func (s *Service) Start(ctx context.Context) {
-	s.graph.StartHealthCheckLoop(ctx, time.Second*5)
-
-	// TODO
-	//s.startExtractLoop(ctx)
-	s.extract()
-}
-
-func (s *Service) startExtractLoop(ctx context.Context) {
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				s.extract()
-			case <-ctx.Done():
-				log.Println("extract loop done")
-				return
-			}
-		}
-	}()
-}
-
-func (s *Service) extract() {
-	for _, ex := range s.extractors {
-		ex.Reset()
-		for ex.HasNextVertex() {
-			v := ex.NextVertex()
-			s.graph.NewVertex(v.Key, v.Label, v.Healthy)
-			log.Println("Vertex:", v.Key, v.Label, v.Healthy)
-		}
-		for ex.HasNextEdge() {
-			e := ex.NextEdge()
-			s.graph.NewEdge(e.Source, e.Target)
-			log.Println("Edge:", e.Source, e.Target)
-		}
-	}
-}
-
 func (s *Service) GetVertex(key string) (graphlib.Vertex, error) {
-	log.Println("service.GetVertex", key)
-	return s.graph.GetVertex(key)
+	slog.Debug("service.GetVertex", slog.String("key", key))
+
+	v, err := s.graph.GetVertex(key)
+	if err != nil {
+		slog.Error("service.GetVertex", slog.String("key", key), slog.String("error", err.Error()))
+		return graphlib.Vertex{}, err
+	}
+	return v, nil
+}
+
+func (s *Service) GetVertexAttributes(key string) ([]VertexAttribute, error) {
+	slog.Debug("service.GetVertexAttributes", slog.String("key", key))
+
+	_, err := s.graph.GetVertex(key)
+	if err != nil {
+		slog.Error("service.GetVertexAttributes", slog.String("key", key), slog.String("error", err.Error()))
+		return nil, err
+	}
+
+	return []VertexAttribute{
+		{Type: "string", Description: "Grupo de suporte", Value: "234 - DITI"},
+		{Type: "string", Description: "Gerente", Value: "João Paulo"},
+		{Type: "string", Description: "Cluster", Value: "JAHSGDSALHD"},
+		{Type: "string", Description: "Ambiente", Value: "Produção"},
+		{Type: "link", Description: "Link monitoração", Value: "http://google.com.br"},
+		{Type: "link", Description: "Repositório Github", Value: "http://github.com"},
+		{Type: "link", Description: "Consultar incidentes", Value: "http://google.com.br"},
+	}, nil
 }
 
 func (s *Service) SetVertexHealth(key string, health bool) error {
-	log.Println("service.SetVertexHealth", key, health)
-	return s.graph.SetVertexHealth(key, health)
+	slog.Debug("service.SetVertexHealth", slog.String("key", key), slog.Bool("health", health))
+
+	err := s.graph.SetVertexHealth(key, health)
+	if err != nil {
+		slog.Error("service.SetVertexHealth", slog.String("key", key), slog.String("error", err.Error()))
+		return err
+	}
+	return nil
 }
 
 func (s *Service) ClearGraphHealthyStatus() {
-	log.Println("service.ClearGraphHealthyStatus")
+	slog.Debug("service.ClearGraphHealthyStatus")
 	s.graph.ClearGraphHealthyStatus()
 }
 
 func (s *Service) Summary() Summary {
-	log.Println("service.Summary")
+	slog.Debug("service.Summary")
+
 	stats := s.graph.GraphStats()
 
 	sum := Summary{
-		TotalEdges:        stats.TotalEdges,
-		TotalVertices:     stats.TotalVertices,
+		TotalEdges:    stats.TotalEdges,
+		TotalVertices: stats.TotalVertices,
+
+		TotalHealthyVertices:   stats.TotalHealthyVertices,
+		TotalUnhealthyVertices: stats.TotalUnhealthyVertices,
+
 		UnhealthyVertices: stats.UnhealthyVertices,
 	}
 	return sum
 }
 
 func (s *Service) VertexDependencies(key string, all bool) (QueryResult, error) {
-	log.Println("service.VertexDependencies", key)
+	slog.Debug("service.VertexDependencies", slog.String("key", key), slog.Bool("all", all))
+
 	p, err := s.graph.GetVertex(key)
 	if err != nil {
+		slog.Error("service.VertexDependencies, graph.GetVertex", slog.String("key", key), slog.Bool("all", all), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
 	dep, err := s.graph.VertexDependencies(key, all)
 	if err != nil {
+		slog.Error("service.VertexDependencies, graph.VertexDependencies", slog.String("key", key), slog.Bool("all", all), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
@@ -132,15 +124,18 @@ func (s *Service) VertexDependencies(key string, all bool) (QueryResult, error) 
 	return sub, nil
 }
 
-func (s *Service) GetVertexDependents(key string, all bool) (QueryResult, error) {
-	log.Println("service.GetVertexDependents", key)
+func (s *Service) VertexDependents(key string, all bool) (QueryResult, error) {
+	slog.Debug("service.VertexDependents", slog.String("key", key), slog.Bool("all", all))
+
 	p, err := s.graph.GetVertex(key)
 	if err != nil {
+		slog.Error("service.VertexDependents, graph.GetVertex", slog.String("key", key), slog.Bool("all", all), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
 	dep, err := s.graph.VertexDependents(key, all)
 	if err != nil {
+		slog.Error("service.VertexDependents, graph.VertexDependents", slog.String("key", key), slog.Bool("all", all), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
@@ -152,15 +147,18 @@ func (s *Service) GetVertexDependents(key string, all bool) (QueryResult, error)
 	return sub, nil
 }
 
-func (s *Service) Neighbors(key string) (QueryResult, error) {
-	log.Println("service.Neighbors", key)
+func (s *Service) VertexNeighbors(key string) (QueryResult, error) {
+	slog.Debug("service.VertexNeighbors", slog.String("key", key))
+
 	p, err := s.graph.GetVertex(key)
 	if err != nil {
+		slog.Error("service.VertexNeighbors, graph.GetVertex", slog.String("key", key))
 		return QueryResult{}, err
 	}
 
 	neighbors, err := s.graph.VertexNeighbors(key)
 	if err != nil {
+		slog.Error("service.VertexNeighbors, graph.VertexNeighbors", slog.String("key", key), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
@@ -173,20 +171,23 @@ func (s *Service) Neighbors(key string) (QueryResult, error) {
 }
 
 func (s *Service) Path(kSrc, ktgt string) (QueryResult, error) {
-	log.Println("service.Path", kSrc, ktgt)
+	slog.Debug("service.Path", slog.String("kSrc", kSrc), slog.String("ktgt", ktgt))
 
 	src, err := s.graph.GetVertex(kSrc)
 	if err != nil {
+		slog.Error("service.Path, graph.GetVertex", slog.String("kSrc", kSrc), slog.String("ktgt", ktgt), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
 	tgt, err := s.graph.GetVertex(ktgt)
 	if err != nil {
+		slog.Error("service.Path, graph.GetVertex", slog.String("kSrc", kSrc), slog.String("ktgt", ktgt), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
 	path, err := s.graph.Path(kSrc, ktgt)
 	if err != nil {
+		slog.Error("service.Path, graph.Path", slog.String("kSrc", kSrc), slog.String("ktgt", ktgt), slog.String("error", err.Error()))
 		return QueryResult{}, err
 	}
 
